b8ddeb3a7a7e5b45fa9aa73d92357122a9e163fc
kerBamf <zach.pedigo@gmail.com>
2023-09-21 15:54:47 -0400

    Added comments

--- BAICButton.js
+++ BAICButton.js
@@ -5,15 +5,16 @@ const button2 = 'contentOnly'
 
 xapi.Event.UserInterface.Extensions.Widget.Action.on(async (event) => {
   console.log(event)
-  if (event.WidgetId === button1) {
+  //
+  if (event.WidgetId === button1) { //Changes configs if button 1 is pressed
     xapi.Config.Video.Output.Connector[1].MonitorRole.set('First')
     xapi.Config.Video.Output.Connector[3].MonitorRole.set('First')
-    const mode = await xapi.Config.Video.Output.Connector[1].MonitorRole.get()
-    //console.log(mode)
-  } else if (event.WidgetId === button2) {
+    // const mode = await xapi.Config.Video.Output.Connector[1].MonitorRole.get()
+    // console.log(mode)
+  } else if (event.WidgetId === button2) { //Changes configs if button 2 is pressed
     xapi.Config.Video.Output.Connector[1].MonitorRole.set('PresentationOnly')
     xapi.Config.Video.Output.Connector[3].MonitorRole.set('PresentationOnly')
-    const mode = await xapi.Config.Video.Output.Connector[1].MonitorRole.get()
-    //console.log(mode)
+    // const mode = await xapi.Config.Video.Output.Connector[1].MonitorRole.get()
+    // console.log(mode)
   }
 })
